64ba35b24deabc500fcae9de97f9e7f754dcee78
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2021-09-14 19:43:40 -0500

    removed example from previous hw

--- src/homework/02_expressions/hwexpressions.h
+++ src/homework/02_expressions/hwexpressions.h
@@ -1,8 +1,4 @@
-﻿//example
-int add_numbers(int num1, int num2);
-
-
-const double TAXRATE = 6.75;
+﻿const double TAXRATE = 6.75;
 
 //write function prototype here
 double get_sales_tax(double meal_amount);
