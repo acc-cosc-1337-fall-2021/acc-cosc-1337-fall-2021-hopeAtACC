c90c1c708d976d3f4b8818c80868ac5bee081f66
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2021-09-21 17:32:49 -0500

    added choice for switch function

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -10,10 +10,10 @@ int main()
 	//asks user to select from menu
 
 	//displays menu
-	cout << "MAIN MENU \n\n";
-	cout << "1-Letter grade using if\n";
-	cout << "2-Letter grade using switch\n";
-	cout << "3-Exit\n\n";
+	cout << "MAIN MENU \n\n"
+		 << "1-Letter grade using if\n"
+		 << "2-Letter grade using switch\n"
+		 << "3-Exit\n\n";
 	
 	//asks user for choice, stores their choice
 	cout << "Enter your choice: ";
@@ -30,6 +30,19 @@ int main()
 		else
 		{
 			cout << "The letter grade is: " + get_letter_grade_using_if(grade);
+		}
+	}
+	else if (choice == 2)
+	{
+		cout << "Please enter a numerical grade from 0 - 100: ";
+		cin >> grade;
+		if (grade < 0 || grade > 100)
+		{
+			cout << "That number is out of range.  Please enter a number from 0 - 100: ";
+		}
+		else
+		{
+			cout << "The letter grade is: " + get_letter_grade_using_switch(grade);
 		}
 	} 
 	else if (choice == 3)
