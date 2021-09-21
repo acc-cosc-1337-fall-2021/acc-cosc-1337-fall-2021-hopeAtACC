c63d711728803e548de1a07d53251f12e956c1b5
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2021-09-21 09:46:07 -0500

    created if statement fxn returning letter grade

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,30 @@
 //write include statement for decisions header
+#include "decisions.h"
 
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int grade)
+{
+    if (grade >= 90) 
+    {
+        return "A";
+    }
+    else if (grade >= 80)
+    {
+        return "B";
+    }
+    else if (grade >= 70)
+    {
+        return "C";
+    }
+    else if (grade >= 60)
+    {
+        return "D";
+    }
+    else 
+    {
+        return "F";
+    }
+
+};
+
