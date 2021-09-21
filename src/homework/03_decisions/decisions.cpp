ae1d766f3a31d457dbe8f41c46a42ef8762aa466
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2021-09-21 17:32:22 -0500

    added switch function

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -28,3 +28,29 @@ string get_letter_grade_using_if(int grade)
 
 };
 
+string get_letter_grade_using_switch(int grade)
+{
+    string letterGrade;
+
+    switch (grade/10)
+    {
+        case 10:
+        case 9:
+            letterGrade = "A";
+            break;
+        case 8: 
+            letterGrade = "B";
+            break;
+        case 7: 
+            letterGrade = "C";
+            break;
+        case 6: 
+            letterGrade = "D";
+            break;
+        default: 
+            letterGrade = "F";
+    }
+
+    return letterGrade;
+}
+
