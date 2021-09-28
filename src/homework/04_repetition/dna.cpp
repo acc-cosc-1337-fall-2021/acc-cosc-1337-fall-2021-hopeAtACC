08f446bf2ae008014335ed905dcc51790d78e2a2
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2021-09-28 11:11:37 -0500

    added factorial and gcd functions

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,39 @@
 //add include statements
+#include "dna.h"
 
-//add function(s) code here
+//factorial function
+int factorial (int num)
+{   
+    int product = 1;
+ 
+    while (num > 0)
+    {
+        product = product * num;
+        num--;
+    };
+
+    return product;
+};
+
+
+//greatest common divisor function
+int gcd (int num1, int num2)
+{
+
+    while (num1 != num2)
+    {
+        if (num1 < num2)
+        {
+            int num3 = num1;
+            num3 = num1;
+            num1 = num2;
+            num2 = num3;
+        }
+        if (num1 > num2)
+        {
+            num1 = num1 - num2;
+        }    
+    }
+    return num1;
+
+}
