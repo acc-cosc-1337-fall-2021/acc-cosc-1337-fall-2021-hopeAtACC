8d8416ae793ff5b9e69fcdae1ac931f516c87117
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2021-10-07 16:51:13 -0500

    created menu choices

--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,14 +1,41 @@
-/*
-use a vector of int with values 8, 4, 20, 88, 66, 99
+//write include statements
+#include "func.h"
+//write using statements
+using std::cout; using std::cin; using std::endl;
 
-Prompt user for 1 for Get Max from vector and 2 for Get primes.
-Prompt user for a number and return max value or list of primes
-and display them to screen.
-Program continues until user decides to exit.
-
-*/
 int main() 
-{
+{	
+	int choice;
+	string dna;
+
+	do
+	{
+		cout << endl << "Choose one of the following perform: \n"
+			 <<	"1 - Get GC Content \n"
+			 <<	"2 - Get DNA Complement \n"
+			 <<	"3 - Exit \n" << endl;
+		cin >> choice;
+
+		if (choice == 1) 
+		{
+			cout << endl << "Please type a string of DNA to find the GC content: ";
+			cin >> dna;
+			cout << endl << "The GC content of " << dna << " is " << get_gc_content(dna) << endl;
+		}
+		else if (choice == 2)
+		{
+			cout << endl <<  "Please type a string of DNA to get the DNA complement: ";
+			cin >> dna;
+			cout << endl << "The DNA complement of " << dna << " is: " << get_dna_complement(dna) << endl;
+		}
+		else if (choice == 3)
+		{
+			exit(0);
+		}
+	}
+
+	while (choice != 1 || choice != 2);
 
 	return 0;
+
 }
