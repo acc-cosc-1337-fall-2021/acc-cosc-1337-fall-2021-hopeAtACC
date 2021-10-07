9d8b17d3a7127b576f5d51653b4577001367882c
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2021-10-07 09:58:16 -0500

    added prototype functions

--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,12 @@
-//add function(s) prototype here
+//add include statements
+#include <iostream>
+#include <string>
+
+using std::string;
+
+
+double get_gc_content(const string& dna);
+
+string get_dna_complement(string dna);
+
+string reverse_string(string dna);
