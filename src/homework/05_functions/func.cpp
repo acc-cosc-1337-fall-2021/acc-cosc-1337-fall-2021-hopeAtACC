169b8e5eab483f7cd5c37813b00250460a91a776
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2021-10-08 12:17:14 -0500

    added comments

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -7,21 +7,23 @@ using std::string; using std::cout;
 double get_gc_content(const string& dna)
 {
     double gc = 0;
-    
+    //loops to find all G or C characters in dna
     for (int i = 0; i < dna.length(); i++)
     {
         if (dna[i] == 'G' || dna[i] == 'C')
         {
+            //adds to gc quantity if G or C exists in that index
             gc += 1;
         }
-    }  
+    }
+    //returns calcualted ratio of GC charcters to overall string characters 
     return gc/dna.length(); 
 };
 
 string reverse_string(string dna)
 {
     string reverse = "";
-    
+    //loops through dna string adding the end index chracter to a reverse string each time
     for(int i = dna.length() - 1; i >= 0 ; i--)
     {
         reverse = reverse + dna[i];  
@@ -32,8 +34,9 @@ string reverse_string(string dna)
 string get_dna_complement(string dna)
 {
     string complement = "";
+    //starts with the reversed string returned from reverse function
     string reversed = reverse_string(dna);
-
+    //for a character in reversed it adds its complement to new complement string
     for (int i = 0; i < reversed.length(); i++)
     {
         if (reversed[i] == 'A')
