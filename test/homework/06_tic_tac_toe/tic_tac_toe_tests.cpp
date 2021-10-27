c23ea3b44d765f17bcb68f822848da257f857ae9
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2021-10-27 17:12:23 -0500

    test cases added, passed

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -2,26 +2,150 @@
 #include "tic_tac_toe.h"
 #include "catch.hpp"
 
-TEST_CASE("Verify Tic_tac_toe") {
+TEST_CASE("Verify first player for X") {
+	Tic_tac_toe object;
+	object.start_game("X");
+	REQUIRE(object.get_player() == "X");
+}
+
+TEST_CASE("Verify first player for O") {
+	Tic_tac_toe object;
+	object.start_game("O");
+	REQUIRE(object.get_player() == "O");
+}
+
+TEST_CASE("Verify win by first column") {
 	Tic_tac_toe object;
 	object.start_game("X");
 	object.mark_board(1);
-	REQUIRE(object.game_over() == false);
 	object.mark_board(2);
-	REQUIRE(object.game_over() == false);
-	object.mark_board(3);
-	REQUIRE(object.game_over() == false);
 	object.mark_board(4);
-	REQUIRE(object.game_over() == false);
-	object.mark_board(5);
-	REQUIRE(object.game_over() == false);
-	object.mark_board(6);
-	REQUIRE(object.game_over() == false);
+	object.mark_board(3);
 	object.mark_board(7);
-	REQUIRE(object.game_over() == false);
+	object.mark_board(5);
+	object.mark_board(6);
 	object.mark_board(8);
-	REQUIRE(object.game_over() == false);
 	object.mark_board(9);
 	REQUIRE(object.game_over() == true);
+}
+
+TEST_CASE("Verify win by second column") {
+	Tic_tac_toe object;
+	object.start_game("X");
+	object.mark_board(2);
+	object.mark_board(1);
+	object.mark_board(5);
+	object.mark_board(4);
+	object.mark_board(8);
+	object.mark_board(3);
+	object.mark_board(7);
+	object.mark_board(6);
+	object.mark_board(9);
+	REQUIRE(object.game_over() == true);
+}
+
+TEST_CASE("Verify win by third column") {
+	Tic_tac_toe object;
+	object.start_game("X");
+	object.mark_board(3);
+	object.mark_board(2);
+	object.mark_board(6);
+	object.mark_board(1);
+	object.mark_board(9);
+	object.mark_board(5);
+	object.mark_board(4);
+	object.mark_board(8);
+	object.mark_board(7);
+	REQUIRE(object.game_over() == true);
+}
+
+TEST_CASE("Verify win by first row") {
+	Tic_tac_toe object;
+	object.start_game("X");
+	object.mark_board(1);
+	object.mark_board(9);
+	object.mark_board(2);
+	object.mark_board(6);
+	object.mark_board(3);
+	object.mark_board(5);
+	object.mark_board(4);
+	object.mark_board(8);
+	object.mark_board(7);
+	REQUIRE(object.game_over() == true);
+}
+
+TEST_CASE("Verify win by second row") {
+	Tic_tac_toe object;
+	object.start_game("X");
+	object.mark_board(4);
+	object.mark_board(1);
+	object.mark_board(5);
+	object.mark_board(9);
+	object.mark_board(6);
+	object.mark_board(2);
+	object.mark_board(3);
+	object.mark_board(8);
+	object.mark_board(7);
+	REQUIRE(object.game_over() == true);
+}
+
+TEST_CASE("Verify win by third row") {
+	Tic_tac_toe object;
+	object.start_game("X");
+	object.mark_board(7);
+	object.mark_board(4);
+	object.mark_board(8);
+	object.mark_board(1);
+	object.mark_board(9);
+	object.mark_board(5);
+	object.mark_board(6);
+	object.mark_board(2);
+	object.mark_board(3);
+	REQUIRE(object.game_over() == true);
+}
+
+TEST_CASE("Verify win from diagonally top left") {
+	Tic_tac_toe object;
+	object.start_game("X");
+	object.mark_board(1);
+	object.mark_board(7);
+	object.mark_board(5);
+	object.mark_board(4);
+	object.mark_board(9);
+	object.mark_board(8);
+	object.mark_board(6);
+	object.mark_board(2);
+	object.mark_board(3);
+	REQUIRE(object.game_over() == true);
+}
+
+TEST_CASE("Verify win from diagonally top right") {
+	Tic_tac_toe object;
+	object.start_game("X");
+	object.mark_board(3);
+	object.mark_board(1);
+	object.mark_board(5);
+	object.mark_board(4);
+	object.mark_board(7);
+	object.mark_board(9);
+	object.mark_board(8);
+	object.mark_board(6);
+	object.mark_board(2);
+	REQUIRE(object.game_over() == true);
+}
+
+TEST_CASE("Verify Tic_tac_toe game_over") {
+	Tic_tac_toe object;
+	object.start_game("X");
+	object.mark_board(1);
+	object.mark_board(3);
+	object.mark_board(7);
+	object.mark_board(9);
+	object.mark_board(6);
+	object.mark_board(4);
+	object.mark_board(5);
+	object.mark_board(2);
+	object.mark_board(8);
+	REQUIRE(object.get_winner() == "C");
 }
 
