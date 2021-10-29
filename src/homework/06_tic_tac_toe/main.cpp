2ea2149346528f387a50bfc588631ccfaa248fc0
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2021-10-29 14:00:37 -0500

    spaced out lines

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -49,6 +49,7 @@ int main()
 		}
 		//clears out array for the next round by filling it with zeroes again
 		std::fill( std::begin(pegChecker), std::end(pegChecker), 0 );
+		
 		if (game.get_winner() == "X" || game.get_winner() == "O") {
 			cout << "Congratulations, " << game.get_winner() << ", you won!" << endl;
 		}
