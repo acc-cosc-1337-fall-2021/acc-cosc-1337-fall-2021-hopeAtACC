132c9e42e2233254934d5983f208a71458d1aa1f
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2021-11-02 11:04:05 -0500

    removed input collection/validation from user

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,11 +1,12 @@
 #include "tic_tac_toe.h"
+#include "tic_tac_toe_manager.h"
 
 using std::cout; using std::cin; using std::vector; using std::endl;
 
 int main() 
 {
-	//create object of Tic_tac_toe class
-	Tic_tac_toe game;
+
+	Tic_tac_toe_manager manager;
 
 	string first_player;
 	int pegChecker[9] = {0};//this will check for repeated positions later
@@ -13,7 +14,9 @@ int main()
 	char choice = 'Y';
 
 	do{
-		
+
+		Tic_tac_toe game;
+
 		do
 		{
 			cout << "Choose X or O to start Game: ";
@@ -25,30 +28,13 @@ int main()
 		
 		while(game.game_over() == false)
 		{
-			do
-			{
-				cout << "Choose a board position from 1 - 9: ";
-				cin >> position;
 
-			} while (position > 9 || position < 1);//keep asking user question if they don't provide an integer from 1-9
 
-			//keep asking user to type a position if they type one already in pegChecker array
-			//if they already did that index will have a value of 1
-			while (pegChecker[position - 1] == 1) {
-				cout << "That position has already been chosen.  Please pick a new one: ";
-				cin >> position;
-			}
-		
-			game.mark_board(position);
 
-			//change index position - 1 from a zero to a one in pegChecker array
-			pegChecker[position - 1] = 1;
-			
-			game.display_board();
+
+
 			
 		}
-		//clears out array for the next round by filling it with zeroes again
-		std::fill( std::begin(pegChecker), std::end(pegChecker), 0 );
 		
 		if (game.get_winner() == "X" || game.get_winner() == "O") {
 			cout << "Congratulations, " << game.get_winner() << ", you won!" << endl;
@@ -56,6 +42,8 @@ int main()
 		else {
 			cout << "It's a tie!" << endl;
 		}
+
+		manager.save_game(game);
 
 		cout << "Would you like to play again? ";
 		cin >> choice;
