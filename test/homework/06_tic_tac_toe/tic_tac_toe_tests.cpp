6d0181500317d23d979f42bfcf4f89e76a09ddf9
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2021-11-04 16:13:13 -0500

    fixed unpassing test cases

--- test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
+++ test/homework/06_tic_tac_toe/tic_tac_toe_tests.cpp
@@ -245,48 +245,72 @@ TEST_CASE("Verify Tic_tac_toe_manager get_winner_total for tie") {
 	REQUIRE(x == 0);
 	REQUIRE(t == 1);
 }
-
 TEST_CASE("Verify Tic_tac_toe_manager get_winner_total for x") {
-	Tic_tac_toe_manager manager;
 	Tic_tac_toe object;
+	object.start_game("X");
+	object.mark_board(3);
+	REQUIRE(object.game_over() == false);
+	object.mark_board(1);
+	REQUIRE(object.game_over() == false);
+	object.mark_board(5);
+	REQUIRE(object.game_over() == false);
+	object.mark_board(4);
+	REQUIRE(object.game_over() == false);
+	object.mark_board(7);
+	REQUIRE(object.game_over() == true);
+	object.mark_board(9);
+	REQUIRE(object.game_over() == true);
+	object.mark_board(8);
+	REQUIRE(object.game_over() == true);
+	object.mark_board(6);
+	REQUIRE(object.game_over() == true);
+	object.mark_board(2);
+	REQUIRE(object.game_over() == true);
+	REQUIRE(object.get_winner() == "X");
+	
+	Tic_tac_toe_manager manager;
+
 	int o = 0;
 	int x = 0;
 	int t = 0;
-	object.start_game("X");
-	object.mark_board(3);
-	object.mark_board(1);
-	object.mark_board(5);
-	object.mark_board(4);
-	object.mark_board(7);
-	object.mark_board(9);
-	object.mark_board(8);
-	object.mark_board(6);
-	object.mark_board(2);
-	REQUIRE(object.get_winner() == "X");
+
 	manager.save_game(object);
 	manager.get_winner_total(o, x, t);
 	REQUIRE(o == 0);
 	REQUIRE(x == 1);
 	REQUIRE(t == 0);
+	
 }
 
 TEST_CASE("Verify Tic_tac_toe_manager get_winner_total for o") {
-	Tic_tac_toe_manager manager;
 	Tic_tac_toe object;
+	object.start_game("O");
+	object.mark_board(3);
+	REQUIRE(object.game_over() == false);
+	object.mark_board(1);
+	REQUIRE(object.game_over() == false);
+	object.mark_board(5);
+	REQUIRE(object.game_over() == false);
+	object.mark_board(4);
+	REQUIRE(object.game_over() == false);
+	object.mark_board(7);
+	REQUIRE(object.game_over() == true);
+	object.mark_board(9);
+	REQUIRE(object.game_over() == true);
+	object.mark_board(8);
+	REQUIRE(object.game_over() == true);
+	object.mark_board(6);
+	REQUIRE(object.game_over() == true);
+	object.mark_board(2);
+	REQUIRE(object.game_over() == true);
+	REQUIRE(object.get_winner() == "O");
+	
+	Tic_tac_toe_manager manager;
+
 	int o = 0;
 	int x = 0;
 	int t = 0;
-	object.start_game("O");
-	object.mark_board(3);
-	object.mark_board(1);
-	object.mark_board(5);
-	object.mark_board(4);
-	object.mark_board(7);
-	object.mark_board(9);
-	object.mark_board(8);
-	object.mark_board(6);
-	object.mark_board(2);
-	REQUIRE(object.get_winner() == "O");
+
 	manager.save_game(object);
 	manager.get_winner_total(o, x, t);
 	REQUIRE(o == 1);
