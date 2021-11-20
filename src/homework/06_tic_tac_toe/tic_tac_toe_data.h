02a7eb7b03286bdac0d09cf108e2df8c82cc0acf
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2021-11-19 18:57:56 -0600

    created class

--- src/homework/06_tic_tac_toe/tic_tac_toe_data.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_data.h
@@ -1,1 +1,25 @@
-//h
+#ifndef tic_tac_toe_data_h
+#define tic_tac_toe_data_h
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "tic_tac_toe.h"
+#include "tic_tac_toe_3.h"
+#include "tic_tac_toe_4.h"
+#include <fstream>
+
+using std::unique_ptr; using std::vector; using std::unique_ptr;
+
+
+class Tic_tac_toe_data {
+
+    public:
+
+    void save_games(const vector<unique_ptr<Tic_tac_toe>>& games);
+
+    vector<unique_ptr<Tic_tac_toe>> get_games();
+
+};
+
+#endif
