baaf359560cfbd29fcc88ac873cbb1dc61615010
Hope Watson <89404775+hopeAtACC@users.noreply.github.com>
2021-11-19 18:58:51 -0600

    included methods for data class

--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <vector>
 #include "tic_tac_toe.h"
+#include "tic_tac_toe_data.h"
 
 using std::string; using std::vector; using std::unique_ptr;
 
@@ -17,6 +18,7 @@ class Tic_tac_toe_manager {
         int o_win = 0;
         int ties = 0;
         vector<unique_ptr<Tic_tac_toe>> games;
+        Tic_tac_toe_data data;
 
         //private member functions
         void update_winner_count(string winner);
@@ -28,6 +30,22 @@ class Tic_tac_toe_manager {
         void save_game(unique_ptr<Tic_tac_toe>& obj);
         friend ostream& operator<<(std::ostream& out, Tic_tac_toe_manager& manager);
         void get_winner_total(int& o, int& w, int& t);
+
+        //constructor
+        Tic_tac_toe_manager() = default;
+
+        Tic_tac_toe_manager(Tic_tac_toe_data& d) {
+            data = d;
+            games = d.get_games();
+            for (const auto& game : games) {
+                this->update_winner_count(game->get_winner());
+            }
+        }
+
+        //destructor
+        ~Tic_tac_toe_manager() {
+            data.save_games(games);
+        };
 
 };
 
